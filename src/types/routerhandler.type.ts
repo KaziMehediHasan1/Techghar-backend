264677bb08271df98818ee7ab31e079388983ccc
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-29 22:44:25 +0600

    define,fun: response funtion and types is defined

--- src/types/routerhandler.type.ts
+++ src/types/routerhandler.type.ts
@@ -1,5 +1,4 @@
-import type {Response,Request,NextFunction} from "express"
+import type { Response, Request, NextFunction } from "express";
 export interface IRouterHandler<T = any> {
   (req: Request, res: Response, next: NextFunction): T | Promise<T>;
 }
-
---
+++ src/types/sendResponse.type.ts
@@ -0,0 +1,6 @@
+export type TResponse<T> = {
+  statusCode: number;
+  message?: string;
+  success: boolean;
+  data?: T;
+};
