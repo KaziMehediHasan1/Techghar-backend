ce34ddc5af3317848bfece75863d33554917b81a
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-30 23:48:19 +0600

    controller:user create controller setup done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,1 +1,7 @@
-const createUser = 
+import catchAsync from "../../utils/catchAsync.js";
+
+const createUser = catchAsync(async (req, res) => {});
+
+export const userController = {
+  createUser,
+};
