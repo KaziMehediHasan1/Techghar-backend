2b405ecbda9522f8e783a97175783d943f27ff4b
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2026-01-03 12:58:48 +0600

    package:use uuid, alias @

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,11 @@
+import config from "@/src/config/index.js";
+import bcrypt from "bcrypt"
+
 const createUser = async(payload:any)=>{
-    
+    const {name, email, password,role} = payload
+    const salt = Number(config.bcrypt_salt_rounds)
+    // Hash password
+    const hashedPass = await bcrypt.hash(payload.password, salt);
+    payload.password = hashedPass
+
 }
