0496f81bbc79b70db7114d1ede19efc1fd3553e0
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-01-16 23:26:20 +0600

    product: add all product services funtion

---
+++ src/app/modules/products/products.service.ts
@@ -0,0 +1,17 @@
+const createProductIntoDB = async (payload: any) => {};
+
+const getAllProductsIntoDB = async (payload: any) => {};
+
+const getProductIntoDB = async (payload: string) => {};
+
+const deleteProductIntoDB = async (payload: string) => {};
+
+const updateProductIntoDB = async (payload: any) => {};
+
+export const productService = {
+  createProductIntoDB,
+  getAllProductsIntoDB,
+  getProductIntoDB,
+  deleteProductIntoDB,
+  updateProductIntoDB,
+};
