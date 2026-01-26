fdbd31eb1fc027adbd567853b8fa984703798a93
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-01-26 22:31:22 +0600

    brand: create route complete all sec.

--- src/app/modules/brand/brand.controller.ts
+++ src/app/modules/brand/brand.controller.ts
@@ -1,6 +1,16 @@
+import { brandService } from "@/app/modules/brand/brand.service.js";
 import catchAsync from "@/utils/catchAsync.js";
+import sendResponse from "@/utils/sendResponse.js";
 
-const createBrandData = catchAsync(async (req, res) => {});
+const createBrandData = catchAsync(async (req, res) => {
+  const result = await brandService.createBrandDataIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: 201,
+    message: "Brand Added Successfull",
+    success: true,
+    data: result, // just for check
+  });
+});
 
 const getAllBrandData = catchAsync(async (req, res) => {});
 
--- src/app/modules/brand/brand.service.ts
+++ src/app/modules/brand/brand.service.ts
@@ -1,4 +1,17 @@
-const createBrandDataIntoDB = async (payload: any) => {};
+import brandModel from "@/app/modules/brand/brand.model.js";
+import AppError from "@/utils/appError.js";
+
+const createBrandDataIntoDB = async (payload: any) => {
+  const checkBrandIsExist = await brandModel.find(payload);
+  if (checkBrandIsExist) {
+    throw new AppError(400, "already have, add another one");
+  }
+  const result = await brandModel.create(payload);
+  if (!result) {
+    throw new AppError(404, "Not create brand data");
+  }
+  return result;
+};
 
 const getAllBrandDataIntoDB = async (payload: any) => {};
 
