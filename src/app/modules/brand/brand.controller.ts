8f960103ae59e5b9bc750d304b68afaece099923
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-01-26 22:50:02 +0600

    brand: all service completed

--- src/app/modules/brand/brand.controller.ts
+++ src/app/modules/brand/brand.controller.ts
@@ -12,13 +12,50 @@ const createBrandData = catchAsync(async (req, res) => {
   });
 });
 
-const getAllBrandData = catchAsync(async (req, res) => {});
+const getAllBrandData = catchAsync(async (_, res) => {
+  const result = await brandService.getAllBrandDataIntoDB();
+  sendResponse(res, {
+    statusCode: 200,
+    message: "Brand get Successfull",
+    success: true,
+    data: result,
+  });
+});
 
-const getBrandData = catchAsync(async (req, res) => {});
+const getBrandData = catchAsync(async (req, res) => {
+  const result = await brandService.getBrandDataIntoDB(req.params.id as string);
+  sendResponse(res, {
+    statusCode: 200,
+    message: "Brand get Successfull",
+    success: true,
+    data: result,
+  });
+});
 
-const updateBrandData = catchAsync(async (req, res) => {});
+const updateBrandData = catchAsync(async (req, res) => {
+  const result = await brandService.updateBrandDataIntoDB({
+    id: req.params.id as string,
+    data: req.body,
+  });
+  sendResponse(res, {
+    statusCode: 200,
+    message: "Brand update data Successfull",
+    success: true,
+    data: result,
+  });
+});
 
-const deleteBrandData = catchAsync(async (req, res) => {});
+const deleteBrandData = catchAsync(async (req, res) => {
+  const result = await brandService.deleteBrandDataIntoDB(
+    req.params.id as string,
+  );
+  sendResponse(res, {
+    statusCode: 200,
+    message: "Brand delete data Successfull",
+    success: true,
+    data: result,
+  });
+});
 
 export const brandController = {
   createBrandData,
--- src/app/modules/brand/brand.service.ts
+++ src/app/modules/brand/brand.service.ts
@@ -13,13 +13,41 @@ const createBrandDataIntoDB = async (payload: any) => {
   return result;
 };
 
-const getAllBrandDataIntoDB = async (payload: any) => {};
+const getAllBrandDataIntoDB = async () => {
+  const result = await brandModel.find();
+  if (!result) {
+    throw new AppError(404, "Not get all brands data");
+  }
+  return result;
+};
 
-const getBrandDataIntoDB = async (payload: any) => {};
+const getBrandDataIntoDB = async (payload: any) => {
+  const result = await brandModel.findById({ _id: payload });
+  if (!result) {
+    throw new AppError(404, "Not get brand data");
+  }
+  return result;
+};
 
-const updateBrandDataIntoDB = async (payload: any) => {};
+const updateBrandDataIntoDB = async (payload: any) => {
+  const result = await brandModel.findByIdAndUpdate(
+    { _id: payload.id },
+    { $set: payload.data },
+    { new: true },
+  );
+  if (!result) {
+    throw new AppError(404, "Not updated brand data");
+  }
+  return result;
+};
 
-const deleteBrandDataIntoDB = async (payload: any) => {};
+const deleteBrandDataIntoDB = async (payload: string) => {
+  const result = await brandModel.findByIdAndDelete({ _id: payload });
+  if (!result) {
+    throw new AppError(404, "Not deletion brand data");
+  }
+  return result;
+};
 
 export const brandService = {
   createBrandDataIntoDB,
