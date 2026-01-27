178afc6590af0e4661b089b9593732edeacebd4d
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-01-27 21:00:56 +0600

    blog: blog create functionality completed

--- src/app/modules/blog/blog.controller.ts
+++ src/app/modules/blog/blog.controller.ts
@@ -1,6 +1,16 @@
+import { blogService } from "@/app/modules/blog/blog.service.js";
 import catchAsync from "@/utils/catchAsync.js";
+import sendResponse from "@/utils/sendResponse.js";
 
-const createBlog = catchAsync(async (req, res) => {});
+const createBlog = catchAsync(async (req, res) => {
+  const result = await blogService.createBlogIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: 201,
+    message: "blog created successfully",
+    success: true,
+    data: result,
+  });
+});
 
 const getAllBlog = catchAsync(async (req, res) => {});
 
--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -1,6 +1,21 @@
-const createBlogIntoDB = async (payload: any) => {};
+import blogModel from "@/app/modules/blog/blog.model.js";
+import AppError from "@/utils/appError.js";
 
-const getAllBlogIntoDB = async (payload: any) => {};
+const createBlogIntoDB = async (payload: any) => {
+  const result = await blogModel.create(payload);
+  if (!result) {
+    throw new AppError(404, "not created blogs");
+  }
+  return result;
+};
+
+const getAllBlogIntoDB = async (payload: any) => {
+  const result = await blogModel.create(payload);
+  if (!result) {
+    throw new AppError(404, "not created blogs");
+  }
+  return result;
+};
 
 const getBlogIntoDB = async (payload: any) => {};
 
