1071a76743ac524a6b6f26a075db1381dbffba20
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-02-12 23:14:19 +0600

    contact: delete validation for create contanct data

--- src/app/modules/category/category.interface.ts
+++
@@ -1,9 +0,0 @@
-import type mongoose from "mongoose";
-
-export interface IContact {
-  name: string;
-  email: string;
-  userId: mongoose.Schema.Types.ObjectId;
-  phone: string;
-  description: string;
-}
--- src/app/modules/contact/contact.route.ts
+++ src/app/modules/contact/contact.route.ts
@@ -1,12 +1,10 @@
 import { validateAccessToken } from "@/app/middlewares/auth.js";
-import { validateRequest } from "@/app/middlewares/validateRequest.js";
-import { zodContactValidation } from "@/app/modules/category/category.validation.js";
 import { contactController } from "@/app/modules/contact/contact.controller.js";
 import express from "express";
 
 const route = express.Router();
 
-route.post("/", validateAccessToken("admin"),validateRequest(zodContactValidation),contactController.createContactData);
+route.post("/", validateAccessToken("admin"),contactController.createContactData);
 route.get("/:id", validateAccessToken("user", "admin"),contactController.getContactData);
 route.get("/", validateAccessToken("admin"), contactController.getContactsData);
 route.delete("/:id", validateAccessToken("admin"), contactController.deleteContactData);
