97408b51dec3068acf76bc1c8f946088d664b262
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-03-18 21:37:43 +0600

    add payment intent funtions

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -6,6 +6,7 @@ import express from "express";
 
 const route = express.Router();
 
+route.post("/intent",validateAccessToken("user"),paymentController.createPayment),
 route.post("/",validateAccessToken("user"),validateRequest(zododPaymentValidation),paymentController.createPayment),
 route.get("/",validateAccessToken("admin"), paymentController.getAllPaymentsData),
 route.get("/:id",validateAccessToken("admin","user"),paymentController.getPaymentData),
