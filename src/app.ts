5840bec6932c6103f929949af56b28ca01ac0bab
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-03-26 23:43:46 +0600

    add new frontend url add origin filed

--- src/app.ts
+++ src/app.ts
@@ -17,8 +17,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:5000",
+      "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
