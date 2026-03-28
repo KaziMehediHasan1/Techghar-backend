1b0f72f9f43bf8e68fad05283107b7b96a751ea4
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-03-28 11:57:04 +0600

    set cookies refreshToken

--- src/helper/setRefreshTokenCookie.ts
+++ src/helper/setRefreshTokenCookie.ts
@@ -3,7 +3,7 @@ import { type Response } from "express";
 export const setRefreshTokenCookie = (res: Response, refreshToken: string) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: config.env === "development", // it will change when uses production
+    secure: config.env === "production", // it will change when uses production
     sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 DAYS
   });
