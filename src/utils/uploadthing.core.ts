7ec94c182114341880af10efd2395e9e9e0b1479
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-03-29 16:16:08 +0600

    uploadtingn token are add

--- src/utils/uploadthing.core.ts
+++ src/utils/uploadthing.core.ts
@@ -14,9 +14,9 @@ export const ourFileRouter = {
       return {};
     })
     .onUploadComplete(({ file }) => {
-
       console.log("✅ Upload complete! URL:", file.url);
       console.log("✅ Upload complete! URL:", file);
+      return { url: file.url };
     }),
 } satisfies FileRouter;
 
